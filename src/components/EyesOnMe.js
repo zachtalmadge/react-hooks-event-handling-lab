8786d1ea7e84288571c22895f8a55ad7ec2d5d41
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-10-04 11:03:36 -0400

    deliverables delivered

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+
+const EyesOnMe = () => {
+
+    let handleFocusEvent = () => {
+        console.log('Good!')
+    }
+
+    let handleBlurEvent = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    return (
+        <button onFocus={handleFocusEvent} onBlur={handleBlurEvent}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+
+    const handleInput = (e) => console.log('Entering password...') 
+
     return (
-        <div></div>
+        <div>
+            <input onChange={handleInput} type="password" />
+        </div>
     )
 }
 
